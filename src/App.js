35aa30ebeb3677c31561b418a08e80f2483edf3a
sohil-lalakiya <sohillalakiya@gmail.com>
2023-02-02 20:46:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import WeatherCard from './component/weatherCard';
 
 function App() {
+  const [searchValue, setSearchValue] = useState("Surat");
+  const [weatherInfo, setWeatherInfo] = useState({})
+
+  const getWeatherInfo = async () => {
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=63e9e07a1829937050ac9d51d67f8741`
+      const data = await fetch(url);
+      const res = await data.json();
+
+      const { temp, humidity, pressure } = res.main;
+      const { main: weatherMood } = res.weather[0];
+      const { name: cityName } = res;
+      const { speed: windSpeed } = res.wind;
+      const { country, sunset } = res.sys;
+
+      setWeatherInfo({
+        temp, humidity, pressure, weatherMood, cityName, windSpeed, country, sunset
+      });
+
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getWeatherInfo();
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="wrap">
+        <div className="search">
+          <input
+            type="search"
+            placeholder="search..."
+            autoFocus
+            id="search"
+            className="searchTerm"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+
+          <button
+            className="searchButton"
+            type="button"
+            onClick={() => getWeatherInfo()}
+          >
+            Search
+          </button>
+        </div>
+      </div>
+      <WeatherCard data={weatherInfo} />
     </div>
+
   );
 }
 
