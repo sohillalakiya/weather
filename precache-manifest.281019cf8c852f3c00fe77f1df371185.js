2fe344d032759b91ff8ac27c7b99900183033854
sohil-lalakiya <sohillalakiya@gmail.com>
2023-02-03 17:26:57 +0530

    Updates

---
+++ precache-manifest.281019cf8c852f3c00fe77f1df371185.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab3d079f40330b9d8c4a",
+    "url": "/weather/static/css/main.bac50ef2.chunk.css"
+  },
+  {
+    "revision": "ab3d079f40330b9d8c4a",
+    "url": "/weather/static/js/main.ab3d079f.chunk.js"
+  },
+  {
+    "revision": "205849d973eaa941ab0f",
+    "url": "/weather/static/js/1.205849d9.chunk.js"
+  },
+  {
+    "revision": "5b2e473e16f1bffc6578",
+    "url": "/weather/static/js/2.5b2e473e.chunk.js"
+  },
+  {
+    "revision": "bbd488a7469c565317e0",
+    "url": "/weather/static/js/runtime~main.bbd488a7.js"
+  },
+  {
+    "revision": "4cc9fb62a051ff24288ccd69a7447f80",
+    "url": "/weather/index.html"
+  }
+];
